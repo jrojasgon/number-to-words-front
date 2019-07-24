da9f94b1e3808e646f1a882624533546b531446f
Johanna ROJAS <johannarojasgon@gmail.com>
2019-07-24 13:12:50 +0200

    add constants

---
+++ src/app/AppConstants.ts
@@ -0,0 +1,5 @@
+import { environment } from "../environments/environment";
+
+export class AppConstants {
+    public static SERVICE_ENDPOINT: string = environment.endpointUrl
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import { AppConstants } from './AppConstants';
+
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Number to words'
+  title = 'Numbers to words'
   public convertForm: FormGroup
   public showLoading = false
   public showError = false
@@ -26,21 +28,23 @@ export class AppComponent implements OnInit {
       numberInput: ['', Validators.required]
     });
 
-    this.convertForm.controls['numberInput'].valueChanges
-      .subscribe(() => {
-        this.convert()
-      });
+    this.convertForm.valueChanges.pipe(
+      debounceTime(100)
+    ).subscribe(val => {
+      this.convert()
+    });
   }
 
   convert() {
     let value: number = this.convertForm.get('numberInput').value
     this.showError = false;
+    this.result = ""
     if (this.convertForm.valid) {
       if (this.validateValues(value)) {
         this.showLoading = true
         this.showError = false
 
-        this.httpClient.get<ResultResponse>("http://localhost:9009/convertToWord/" + value, { responseType: 'json' })
+        this.httpClient.get<ResultResponse>(AppConstants.SERVICE_ENDPOINT + value, { responseType: 'json' })
           .subscribe(data => {
             this.result = data.result
             this.showError = data.hasError
@@ -52,7 +56,7 @@ export class AppComponent implements OnInit {
       }
     }
   }
- 
+
   validateValues(value: number): boolean {
     return value >= -2147483648 && value <= 2147483648
   }
